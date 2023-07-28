e13e20240eb5d8c9161dacbcbf0e2e6a63a48edf
Daksh Sardana <daksh2003sardana@gmail.com>
2023-07-28 11:29:48 +0000

    [LeetCode Sync] Runtime - 44 ms (5.28%), Memory - 9.7 MB (100.00%)

---
+++ my-folder/1168-duplicate-zeros/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int i=0;
+        int n=arr.size();
+        while(i<n){
+            if(arr[i]==0){
+                arr.insert(arr.begin()+i, 0);
+                i += 2;
+            }
+            else
+                i++;
+        }
+        arr.resize(n);
+    }
+};
