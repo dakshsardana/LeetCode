443f2039e85d0829e426085ac690908242f65d03
Daksh Sardana <daksh2003sardana@gmail.com>
2023-08-11 19:33:40 +0000

    Sync LeetCode submission - Single Number (cpp)

--- LeetCode/problems/single_number/solution.cpp
+++ LeetCode/problems/single_number/solution.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> un;
-        for(auto a:nums)un[a]++;
-        for(auto a:un){
-            if(a.second!=2)
-                return a.first;
-        }
-        return -1;
+    //     unordered_map<int,int> un;
+    //     for(auto a:nums)un[a]++;
+    //     for(auto a:un){
+    //         if(a.second!=2)
+    //             return a.first;
+    //     }
+    //     return -1;
+    // }
+    int xorr=0;
+    for(int i=0;i<nums.size();i++){
+        xorr=xorr^nums[i];
+    }
+    return xorr;
     }
 };
