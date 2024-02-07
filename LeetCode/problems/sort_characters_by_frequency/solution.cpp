733efd21c9068be777a1b0853d5a8ce8ff661344
Daksh Sardana <daksh2003sardana@gmail.com>
2024-02-07 16:05:29 +0000

    Sync LeetCode submission - Sort Characters By Frequency (cpp)

---
+++ LeetCode/problems/sort_characters_by_frequency/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int> mp;
+        multimap<int,char> r;
+        string str="";
+
+        for(auto a : s)
+            mp[a]++;
+
+        for(auto a : mp)
+            r.insert({a.second, a.first});
+
+        for(auto it = r.rbegin(); it != r.rend(); ++it)
+            str += string(it->first, it->second);
+        return str;
+    }
+};
+
