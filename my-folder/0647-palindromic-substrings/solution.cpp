444c70c0d5050c35b0ce9f72986e05ae1c7ba527
Daksh Sardana <daksh2003sardana@gmail.com>
2024-03-13 18:16:45 +0000

    [LeetCode Sync] Runtime - 7 ms (70.88%), Memory - 7.6 MB (66.77%)

--- my-folder/0647-palindromic-substrings/solution.cpp
+++ my-folder/0647-palindromic-substrings/solution.cpp
@@ -1,21 +1,25 @@
-class Solution {
-public:
-    int countSubstrings(string s) {
-        int n = s.length(), ans = 0;
-        for (int i = 0; i < n; ++i) {
-            int even = palindromeCount(s, i, i + 1);
-            int odd = palindromeCount(s, i, i);
-            ans += even + odd;
+class Solution 
+{
+private:
+    string t;
+    int check(int l, int r, int ans = 0)
+    {
+        while(l >= 0 and r <= t.size())
+        {
+            if(t[l--] == t[r++]) ans++;
+            else break;
         }
         return ans;
     }
-    int palindromeCount(const string& s, int left, int right) {
-        int count = 0;
-        while (left >= 0 && right < s.length() && s[left] == s[right]) {
-            --left;
-            ++right;
-            ++count;
+public:
+    int countSubstrings(string s) 
+    {
+        int n = size(s), ans = 0;t = s;
+        for(int i = 0; i < n; i++)
+        {
+            ans += check(i,i);    // odd length palindromes
+            ans += check(i,i+1); // even length palindromes
         }
-        return count;
+        return ans;
     }
 };
