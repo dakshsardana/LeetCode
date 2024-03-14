2b07e6756f876c3f45324edda691fe8e9dfb3006
Daksh Sardana <daksh2003sardana@gmail.com>
2024-03-14 10:11:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.6 MB (59.27%)

--- my-folder/0153-find-minimum-in-rotated-sorted-array/solution.cpp
+++ my-folder/0153-find-minimum-in-rotated-sorted-array/solution.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
+int findPivot(vector<int>& nums,int n){
+    int s=0;
+    int e=n-1;
+    if(nums[s]<=nums[e]) return 0;
+    while(s<=e){
+        int mid=s+(e-s)/2;
+        if(((mid-1)>=0) and (nums[mid]<nums[mid-1]))return mid;
+        else if(nums[e]<=nums[mid]) s=mid+1;
+        else if( nums[mid]>=nums[n-1] and nums[s] < nums[mid]) s=mid;
+        else e=mid-1;
+    }
+    return s;
+}
     int findMin(vector<int>& nums) {
-        int low=0;
-        int high=nums.size()-1;
-        int ans=INT_MAX;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[low]<=nums[mid]){
-                ans=min(ans,nums[low]);
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-                ans=min(ans,nums[mid]);
-            }
-
-        }
-    return ans; 
+        int ans=findPivot(nums,nums.size());
+        return nums[ans];
     }
 };
