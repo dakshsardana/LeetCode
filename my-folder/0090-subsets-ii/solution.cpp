843596e004caed29cbefbc9e3d6ecada4d123460
Daksh Sardana <daksh2003sardana@gmail.com>
2024-04-17 06:37:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.6 MB (82.77%)

---
+++ my-folder/0090-subsets-ii/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+void sub(vector<int> &ip, vector<int> &x,vector<vector<int>>&ans,int s){
+            ans.push_back(x);
+            for(int i=s;i<ip.size();i++){
+                if(i != s && ip[i] == ip[i-1]) continue;
+                x.push_back(ip[i]);
+                sub(ip, x, ans,i+1 );
+                x.pop_back();
+                // sub(nums, i+1, temp, ans);
+            }
+            
+        }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> x; 
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        sub(nums, x, ans, 0); 
+        return ans;
+    }
+};
+    
